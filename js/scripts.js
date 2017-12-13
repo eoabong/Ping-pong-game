a73b76fc437533629d070a9332821df6765e9b55
erick oduor <eoabong@gmail.com>
2017-12-13 09:39:00 +0300

    added jquery on my scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,31 @@
+var pingpongArray = [];
+// backend stuff
+function pingPong(inputNumber){
+for(var number = 1; number <=inputNumber; number++){
+  if(number %15 === 0){
+    pingpongArray.push("Ping Pong");
+  }else if (number %5 ===0){
+    pingpongArray.push("pong");
+  }else if (number %3 === 0){
+    pingpongArray.push("ping");
+  }else {
+    pingpongArray.push(number);
+  }
+}
+}
 
+
+
+
+//front-end
+$document.ready(function(){
+  $("form#main-form").submit(function(event){
+    event.preventDefault();
+    var inputNumber = parseInt("input#user-input").val();
+    pingPong(inputNumber);
+// append on HTML form
+    pingpongArray.forEach(function(number){
+      $("#apend-result").append("<li>"+inputNumber+"</li>");
+    });
+  });
+});
